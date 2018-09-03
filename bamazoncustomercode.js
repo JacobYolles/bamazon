aaaa783890369a1c133e73c8351265a6fe7aefe5
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-02 22:32:58 -0600

    added a sufficient amount of snark and extra flexibility

--- bamazoncustomercode.js
+++ bamazoncustomercode.js
@@ -34,6 +34,44 @@ function readProducts() {
     });
   }
 
+  function goAgain() {
+    inquirer.prompt([{
+      type: "confirm",
+      name: "reply",
+      message: "Is there another item you had your eye on?"
+    }]).then(function (ans) {
+      if (ans.reply === true) {
+        prompt();
+    }
+    else {
+      console.log("Thank you so much for a buying my items.")
+      connection.end();
+    }
+  
+    })
+  
+  }
+
+  function goAgainTwo() {
+    inquirer.prompt([{
+      type: "confirm",
+      name: "reply",
+      message: "Would you like to try this again?"
+    }]).then(function (ans) {
+      if (ans.reply === true) {
+        prompt();
+    }
+    else {
+      console.log("Better luck next time, champ.")
+      connection.end();
+    }
+  
+    })
+  
+  }
+
+
+
   function prompt() {
     
     connection.query("SELECT * FROM products", function(err, res) {
@@ -79,7 +117,11 @@ function readProducts() {
 
                   // THIS WILL ALLOW THE USER TO NOT GET THE VALUE WRONG.
                 // connection.end();
+                
+
               return false;
+          
+
             } else{
     
               return true;
@@ -92,7 +134,13 @@ function readProducts() {
     var grandTotal = parseFloat(((res[desiredItem].price)*howMuchDesired).toFixed(2));
 
     //check if quanitity is sufficiently met.
-    if(res[desiredItem].stock_quantity >= howMuchDesired){
+    // if it is not met. 
+    if(res[desiredItem].stock_quantity < howMuchDesired){
+      console.log("Sorry, there's not enough in stock!")
+      goAgainTwo();
+    }
+    // else if it is met.
+   else if(res[desiredItem].stock_quantity >= howMuchDesired){
     
     // create a new query
     //update my table.
@@ -104,9 +152,25 @@ function readProducts() {
        // if we have enough items for the customer proceed:
       // if we can't throw an error, otherwise congratulate the purchase.
      function(err, res){
-        if(err) throw err;
-        console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you as soon as possible.");
-    });
+       if (err) throw (err) 
+           console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you as soon as possible.")
+          
+
+          
+            
+          });
+   
+
+      // if(err) throw err;
+      // console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you as soon as possible.");
+
+      // if{
+      //   console.log("Sorry, there's not enough in stock!");
+      // }
+    
+
+       
+
 
 
     // select all from departments
@@ -129,10 +193,7 @@ function readProducts() {
     //     });
     // })
 
-    // }else{
-    //   console.log("Sorry, there's not enough in stock!");
-    // }
-
+    
     goAgain();    
     }
 
@@ -146,20 +207,3 @@ function readProducts() {
 } // end tag for function prompt.
 
 
-function goAgain() {
-  inquirer.prompt([{
-    type: "confirm",
-    name: "reply",
-    message: "Is there another item you had your eye on?"
-  }]).then(function (ans) {
-    if (ans.reply === true) {
-      prompt();
-  }
-  else {
-    console.log("Thank you so much for a buying my items.")
-    connection.end();
-  }
-
-  })
-
-}
